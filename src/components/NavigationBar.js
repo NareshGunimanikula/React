10fc6d450d12f0ec0cfbfa7f374cbef39a9fc3c1
Naresh Gunimanikula <gunimanikala@gmail.com>
2020-11-11 23:09:39 -0500

    Code Clean Up

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -11,22 +11,7 @@ const NavigationBar = () =>{
             <Nav className="mr-sm-2" inline="true" >
                 <Nav.Link href="/Login" >Log In</Nav.Link>
                 <Nav.Link href="/SignUp">Sign Up</Nav.Link>
-            {/* <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/about-us">Contact Us</Nav.Link>
-            <Nav.Link href="/contact-us">About Us</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown> */}
-            </Nav>
-            {/* <Form inline> */}
-            {/* <FormControl type="text" placeholder="Search" className="mr-sm-2" /> */}
-            
-            {/* <Button variant="outline-success">Search</Button> */}
-            {/* </Form> */}
+            </Nav>           
         </Navbar.Collapse>
     </Navbar>
     );
